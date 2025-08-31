0a6966738021405df3911eaa8118c58fb6479215
raharahimi02020-sketch <raharahimi02020@gmail.com>
2025-08-31 12:41:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ users_table = db.table('users')
 support_table = db.table('support')
 
 # ---------- تنظیمات ----------
-ADMIN_ID = 123456789  # آی‌دی تلگرام ادمین خودت
+ADMIN_ID = 8461153976  # آی‌دی تلگرام ادمین خودت
 
 # ---------- متدهای کمک ----------
 def get_user(user_id):
